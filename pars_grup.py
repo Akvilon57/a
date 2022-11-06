8a5daae1376ecb89638b1f190a7f2a7acd78c69d
Akvilon57 <akvilon_ua@bigmir.net>
2022-11-06 19:22:52 +0200

    teleg

--- pars_grup.py
+++ pars_grup.py
@@ -1,6 +1,6 @@
 import configparser
 import json
-import time
+import time,random,schedule
 
 from telethon.sync import TelegramClient
 from telethon import connection,events
@@ -18,7 +18,12 @@ from telethon import functions, types
 
 # Считываем учетные данные
 
-
+config = configparser.ConfigParser()
+config.read("config.ini")
+api_id   = config['Telegram']['api_id']
+api_hash = config['Telegram']['api_hash']
+username = config['Telegram']['username']
+	
 
 	
 	
@@ -55,13 +60,16 @@ def dump_all_participants(channel):
 		for p in participants.users:
 			print(p.id)
 	
-import time,random
+
 #Парсер сообщений группы Телеграм
 
-def vkusno_poest(client):
+def vkusno_poest():
+	client = TelegramClient('bos', api_id, api_hash)
+	client.start()
 	t=0
 	s=client.iter_messages('topretsept')#zagotovka_na100
 	a=0
+	k=0
 	key=0
 	with open('vkusno.txt', 'r', encoding='utf-8') as file_object:
 	    key=file_object.read()
@@ -72,16 +80,84 @@ def vkusno_poest(client):
 	for i in s:
 		a+=1
 		if a==key:
-			i.text=str(i.text).replace('🔝 https://t.me/topretsept 🔝','https://t.me/vkusno_poest_recepti')
-			client.send_message('vkusno_poest_recepti', i)
-			print(f'Tovar{a}')
+			try:
+				i.text=str(i.text).replace('🔝 https://t.me/topretsept 🔝','https://t.me/vkusno_poest_recepti')
+				client.send_message('vkusno_poest_recepti', i)
+				print(f'Tovar{a}')
+				break
+			except Exception as e:
+				print(f'Error: {e}')
+				if k<3:
+					key+=1
+					k=+1
+				else:
+					break
+			
+		
 
 			
-			with open('vkusno.txt', 'w', encoding='utf-8') as file:
-				file.write(f'{key+1}')
-				file.close()
-			client.run_until_disconnected()	
+	with open('vkusno.txt', 'w', encoding='utf-8') as file:
+		file.write(f'{key+1}')
+		file.close()
+				
 
+	print('disconnect')
+	client.disconnect()
+	print('end')
+			#client.run_until_disconnected()
+
+def anekdot():
+	client = TelegramClient('bos', api_id, api_hash)
+	client.start()
+
+	spis=['AnekdotRu','AnekdotRu','AnekdotRu','AnekdotRu','anekdot18']
+	number=random.randint(0,len(spis)-1)
+	channnel = spis[number]
+	print(channnel)
+
+	k=0
+	t=0
+	s=client.iter_messages(channnel)
+	a=0
+	key=0
+	with open(f'{channnel}.txt', 'r', encoding='utf-8') as file_object:
+	    key=file_object.read()
+	    key=int(key)
+	    file_object.close()
+
+
+	for i in s:
+		a+=1
+		if a==key:
+			try:
+				i.text=str(i.text).replace(f'@{channnel}','https://t.me/anekdot_days')
+				client.send_message('anekdot_days', i)
+				print(f'Tovar{a}')
+				break
+			except Exception as e:
+				print(f'Error: {e}')
+				if k<3:
+					key+=1
+					k=+1
+				else:
+					break
+
+				
+
+
+			
+
+			
+	with open(f'{channnel}.txt', 'w', encoding='utf-8') as file:
+		file.write(f'{key+1}')
+		file.close()
+				
+	#client.run_until_disconnected()
+	print('disconnect')
+	client.disconnect()
+	print('end')
+
+				
 
 
 			
@@ -117,9 +193,8 @@ def vkusno_poest(client):
 
 
 #telethon_on(vkusno_poest())
-import schedule
-import time
-def timer(func):
+
+def timer(vkusno1,anekdot2):
     
     
     #schedule.every(2).minutes.do(func)
@@ -130,26 +205,26 @@ def timer(func):
     #schedule.every().minute.at(":17").do(job)
     #schedule.every(4).days.at("02:00").do(func)
     #schedule.every().days.at("13:30:00").do(func)
-    schedule.every().day.at("10:00").do(func)
-    schedule.every().day.at("18:01").do(func)
+	schedule.every().day.at("07:14").do(vkusno1)
+	schedule.every().day.at("11:13").do(vkusno1)
+	schedule.every().day.at("16:10").do(vkusno1)
+	schedule.every().day.at("07:17").do(anekdot2)
+	schedule.every().day.at("11:17").do(anekdot2)
+	schedule.every().day.at("16:15").do(anekdot2)
 
-    while True:
-        schedule.run_pending()
-    
-        time.sleep(1)
+
+	while True:
+		schedule.run_pending()
+		
+		time.sleep(58)
 
 
 
 if __name__ == '__main__': 
-	config = configparser.ConfigParser()
-	config.read("config.ini")
-	api_id   = config['Telegram']['api_id']
-	api_hash = config['Telegram']['api_hash']
-	username = config['Telegram']['username']
-	client = TelegramClient('bos', api_id, api_hash)
-	client.start()
-	def main():
-		vkusno_poest(client)
 	
-	timer(main)
+	#def main():
+	#	vkusno_poest(client)
+	
+	timer(vkusno_poest,anekdot)
+	
 
